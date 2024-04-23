18d949d0de2d3fcc75cd5b317c23aa34a2ab8d53
shawal-mbalire <mbalireshawal@gmail.com>
2024-04-23 14:03:23 +0300

    near final

--- mySensors/__init__.py
+++ mySensors/__init__.py
@@ -1,11 +1,6 @@
+import board
 import adafruit_dht
-import RPi.GPIO as GPIO
 from picamera2 import Picamera2
 
-DHT_PIN=4
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DHT_PIN, GPIO.IN)
-
-mydht = adafruit_dht.DHT11(DHT_PIN)
+mydht = adafruit_dht.DHT11(board.D4)
 mycamera = Picamera2()
