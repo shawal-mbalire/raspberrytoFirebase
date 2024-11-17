077c27d1b817405c2939b52b62bf54db9fa39405
Shawal Mbalire <mbalireshawal@gmail.com>
2024-11-17 22:14:49 +0300

    Update __init__.py

--- myFirebase/__init__.py
+++ myFirebase/__init__.py
@@ -4,13 +4,13 @@ cred = credentials.Certificate("./myFirebase/creds.json")
 myApp = initialize_app(
     cred,
     {
-        'apiKey': "AIzaSyAi1q5puRg4RqjMpjdQCB5Ozwe0h2BxKTk",
-        'authDomain': "green-coffee-beans-75fe4.firebaseapp.com",
-        'databaseURL': "https://green-coffee-beans-75fe4-default-rtdb.europe-west1.firebasedatabase.app",
-        'projectId': "green-coffee-beans-75fe4",
-        'storageBucket': "green-coffee-beans-75fe4.appspot.com",
-        'messagingSenderId': "392123065",
-        'appId': "1:392123065:web:55f57eb6ccb709ca23c168",
-        'measurementId': "G-VD7KLJHSZY"
+        'apiKey': "",
+        'authDomain': "",
+        'databaseURL': "",
+        'projectId': "",
+        'storageBucket': "",
+        'messagingSenderId': "",
+        'appId': "",
+        'measurementId': ""
     }
 )
